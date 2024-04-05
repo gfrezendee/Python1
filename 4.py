189301539606b7c5c3b9e7c16f61680db1add457
Gabriel Rezende <63487827+gfrezendee@users.noreply.github.com>
2024-04-05 19:30:34 -0300

    4.py

--- 4.py
+++ 4.py
@@ -1,2 +1,12 @@
-a=int(input("Insira um numero que contenha cinco digitos: "))
-print(f"{a}")
+def imprimir_digitos(numero):
+  try:
+    numero = int(numero)
+  except ValueError:
+    print("Número inválido. Digite um número inteiro.")
+    return
+
+  for digito in str(numero):
+    print(digito, end="   ")
+
+numero = input("Digite um número: ")
+imprimir_digitos(numero)
